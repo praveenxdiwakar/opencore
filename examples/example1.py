d31d6b093b49bd8012a0fcc48e67d7bd176fd632
Praveen Diwakar <praveendiwakar.in@gmail.com>
2026-03-08 18:22:15 +0530

    Update example1.py

--- examples/example1.py
+++ examples/example1.py
@@ -1,5 +1,8 @@
-# example1.py
+# example1.py - basic usage example
 
-from src.core import greet
+from src.core import greet, calculate_sum
+from src.utils import reverse_string
 
-print(greet("Open Source Contributor"))
+print(greet("Developer"))
+print("Sum of 5 + 7 =", calculate_sum(5, 7))
+print("Reversed string:", reverse_string("OpenCore"))
