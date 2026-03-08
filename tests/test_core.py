5060cf236fb4896c8100e7305ef81a58b0ac29e5
Praveen Diwakar <praveendiwakar.in@gmail.com>
2026-03-08 18:23:02 +0530

    Update test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,11 +1,16 @@
-# test_core.py
-
 import unittest
-from src.core import greet
+from src.core import greet, calculate_sum, calculate_product
 
 class TestCore(unittest.TestCase):
+
     def test_greet(self):
-        self.assertEqual(greet("Developer"), "Hello, Developer! Welcome to Open Core.")
+        self.assertEqual(greet("Test"), "Hello, Test! Welcome to Open Core.")
+
+    def test_calculate_sum(self):
+        self.assertEqual(calculate_sum(3, 4), 7)
+
+    def test_calculate_product(self):
+        self.assertEqual(calculate_product(3, 4), 12)
 
 if __name__ == "__main__":
     unittest.main()
